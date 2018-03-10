eb8840e0a61c5a3862f367e26d5e17aa19a26c69
rootadmin <kyourselfer@gmail.com>
2018-03-11 01:28:47 +0300

    4_easy.py:Done

--- 4/easy.py
+++ 4/easy.py
@@ -18,15 +18,40 @@
 # + Элемент не кратен 4
 
 # 1
-# l1 = [i for i in range(1, 10, 2)]
-# l1n = l1.copy()
-# l1n = [i ** 2 for i in l1n]
-# print(l1, '\ti ** 2 =\t', l1n)
+print('\n\t\t\t--- Task #1 ---:\n')
+l1 = [i for i in range(1, 10, 2)]
+l1n = l1.copy()
+l1n = [i ** 2 for i in l1n]
+print(l1, '\ti ** 2 =\t', l1n)
+
 
 # 2
-li1 = ['water melon', 'pear', 'melon', 'passion fruit', 'kiwi']
-li2 = ['pear', 'plum', 'passion fruit']
+print('\n\t\t\t--- Task #2 ---:\n')
+fruitsBox1 = ['passion fruit', 'water melon', 'pear', 'melon', 'kiwi']
+fruitsBox2 = ['pear', 'plum', 'passion fruit']
 
-lieq = [i for i in li1 if i == (li2)]
-print(lieq)
+fruitsBoxEq = [x for x in fruitsBox1 if x in fruitsBox2]
+print('Equal fruits from Box 1 and Box 2: ', fruitsBoxEq)
+
+# 3
+print('\n\t\t\t--- Task #3 ---:\n')
+import random
+
+count = 0
+while True:
+    # Генерируем произвольное кол-во элем. с случ. числом
+    ran_gen = [j for j in range(-100, 100, random.randrange(1, 100))]
+    # Выбираем элем. из списка удовлетворяющие условиям ...
+    ran_num = [i for i in ran_gen if (i % 3) == 0 and i == abs(i) and (i % 4) != 0]
+    if ran_num == []:
+        count += 1
+        ran_gen = [j for j in range(-100, 100, random.randrange(1, 100))]
+    else:
+        print('Мы имеем список:\n{}\n'
+              'Елемент(ы) удовлетворяющие условию'
+              ' \"положительный и кратный 3 и не кратный 4\" найден(ы):\n'
+              '\t{}'.format(ran_gen, ran_num))
+        break
+if count > 1:
+    print(2 * '\n\t', 'Кол-во попыток =', count, '\n')
 
